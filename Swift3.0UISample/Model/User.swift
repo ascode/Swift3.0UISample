1df93be189aa465c5df52c2a53588246fdef43ba
arthurkiing <ascode@msn.cn>
2016-09-18 19:40:00 +0800

    Made a extremely simple for CoreData with swift 3.0

---
+++ Swift3.0UISample/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Swift3.0UISample
+//
+//  Created by 金飞 on 16/9/18.
+//  Copyright © 2016年 Fei Jin. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class User: NSManagedObject {
+//    var id : Int!
+//    var name : String!
+}
--- Swift3.0UISample/ViewController.swift
+++ Swift3.0UISample/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let row : AnyObject = NSEntityDescription.insertNewObject(forEntityName: "User", into: context)
+        
+        row.setValue(1, forKey: "id")
+        row.setValue("jinfei", forKey: "name")
+        do{
+            
+            try context.save()
+            
+        }catch let err as NSError{
+            print(err.localizedDescription)
+        }
+        
+        let f : NSFetchRequest<User> = NSFetchRequest(entityName: "User")
+        
+        
+        do {
+            let result = try context.fetch(f)
+            print(result.count)
+            for i in 0 ..< result.count {
+                print(result[i].value(forKey: "name"))
+            }
+            print(result)
+        }catch let err as NSError {
+            print(err.localizedDescription)
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
